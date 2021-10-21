c19d59d49b25c9387f1d4af35350622f00843883
EC2 Default User <ec2-user@ip-172-31-56-103.ec2.internal>
2021-10-21 21:26:17 +0000

    finish

--- controllers/characterControllers.js
+++ controllers/characterControllers.js
@@ -1,19 +1,19 @@
 const Character = require('../models/Character.js');
 
-const postCharacter = (req,res)=>{
+const postCharacter = function (req,res){
     let character = new Character({
         name: req.body.name,
         powers:req.body.powers,
         type: req.body.type,
-        hp: req.body.hp
+        hp: req.body.hp,
         }); 
        
     character.save()
 .then(result=>{
     res.set('content-location', `/api/v1/${character.type === "hero" ? "heroes" : "villains"}/${character._id}`);
     res.status(201).json({
-    data:character,
-    description: character.description,
+    data: character,
+
     url:`/api/v1/${character.type === "hero" ? "heroes" : "villains"}/${character._id}`
 
     })}
@@ -24,9 +24,9 @@ const postCharacter = (req,res)=>{
 
 const getHeroes = (req, res,next) => {
     if (req.params.type === 'heroes'){
-    Character.find({"type":"hero"}).exec()
-        .then(results => {
-            res.status(200).json(results)
+    Character.findByType("hero").exec()
+        .then(allHeroes => {
+            res.status(200).json(allHeroes)
         })
         .catch(error=>res.status(500).send(error))
     } else next();
@@ -34,9 +34,9 @@ const getHeroes = (req, res,next) => {
 
 const getVillains = (req, res) => {
     if(req.params.type === 'villains'){
-    Character.find({"type":"villain"}).exec()
-        .then(results => {
-            res.status(200).json(results)
+    Character.findByType("villain").exec()
+        .then(allVillains => {
+            res.status(200).json(allVillains)
         })
         .catch(error=>res.status(500).send(error))
     }
@@ -46,6 +46,7 @@ const getTheCharacter = (req, res) => {
 Character.findOne({ "_id": req.params.id }).exec()
         .then(results => {
             res.status(200).json(results)
+            
         })
         .catch(error => res.status(500).send(error))
 }
@@ -55,5 +56,4 @@ module.exports = {
     getHeroes,
     getVillains,
     getTheCharacter
-    
 }
--- models/Character.js
+++ models/Character.js
@@ -11,14 +11,18 @@ name:{type:String, required:true, maxLength: 30},
 powers: {type:[String], default: ["Super strength"], set:stringToArray}, 
 type:{type:String, enum:['hero', 'villain'], default:'hero'},
 hp:{type:Number, default:1},
+
 });
 
 CharacterSchema.virtual('description').get(function(){
     return `${this.name} is a ${this.type === 'hero' ? "noble" : "nefarious"} ${this.type} whose special powers are ${this.powers}`
-    
 })
 
+CharacterSchema.static('findByType', function(charType) { return this.find({ "type":charType }); });
 
+CharacterSchema.set('toJSON', {
+    virtuals: true
+});
 const Character = mongoose.model("Character", CharacterSchema); 
 
 module.exports = Character;
